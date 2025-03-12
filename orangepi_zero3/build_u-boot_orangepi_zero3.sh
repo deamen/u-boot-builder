d3c5d2f144c5cb8455082eae58ee7c0db76c2fb6
Song Tang <stang@mmz.au>
2025-03-12 14:13:20 +1100

    Bump up AT-f and u-boot version for orangepi zero3

--- orangepi_zero3/build_u-boot_orangepi_zero3.sh
+++ orangepi_zero3/build_u-boot_orangepi_zero3.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+set -e
 
 # Set atf_ver and uboot_ver environment variables
-export atf_ver="v2.9.0"
-export uboot_ver="v2023.10"
+export atf_ver="lts-v2.10.14"
+export uboot_ver="v2025.01"
 
-export container=$(buildah from arm64v8/alpine:3.18)
+# Alpine Linux 3.21 use swig 4.3 which causes the build to fail, so we use 3.20
+# Need this commit to fix it: https://github.com/u-boot/u-boot/commit/a63456b9191fae2fe49f4b121e025792022e3950
+export container=$(buildah from --arch ARM64 docker.io/library/alpine:3.20)
 buildah config --label maintainer=""github.com/deamen"" $container
 
 # Set atf_ver environment variables in container
@@ -12,17 +16,16 @@ buildah config --env atf_ver=$atf_ver $container
 #Set uboot_ver environment variables in container
 buildah config --env uboot_ver=$uboot_ver $container
 
-buildah run $container apk add git gcc make libc-dev bison flex openssl-dev python3 dtc gcc-arm-none-eabi py3-setuptools swig python3-dev py3-elftools patch
-buildah run $container git clone https://github.com/ARM-software/arm-trusted-firmware.git --depth 1 --branch $atf_ver
-buildah run $container git clone https://github.com/u-boot/u-boot.git --depth 1 --branch $uboot_ver
-buildah run $container git clone https://github.com/Doct2O/orangepi-zero3-bl.git
-
+# gnutls-dev is needed in uboot v2025.01
+buildah run $container apk add git gcc make libc-dev bison flex openssl-dev python3 dtc gcc-arm-none-eabi py3-setuptools swig python3-dev py3-elftools patch gnutls-dev
+buildah run $container git -c advice.detachedHead=false clone https://github.com/ARM-software/arm-trusted-firmware.git --depth 1 --branch $atf_ver
+buildah run $container git -c advice.detachedHead=false clone https://github.com/u-boot/u-boot.git --depth 1 --branch $uboot_ver
+#buildah run $container git clone https://github.com/Doct2O/orangepi-zero3-bl.git
 buildah config --workingdir "/arm-trusted-firmware" $container
 buildah run $container make -j$(nproc --ignore 1) PLAT=sun50i_h616 DEBUG=0 bl31
 buildah config --env BL31="/arm-trusted-firmware/build/sun50i_h616/release/bl31.bin" $container
 
 buildah config --workingdir "/u-boot" $container
-buildah run $container git apply /orangepi-zero3-bl/0001-Defconfig-For-Orangepi-Zero3-PMIC-Env-From-Fat.patch
 
 
 buildah run $container make -j$(nproc --ignore 1) orangepi_zero3_defconfig
